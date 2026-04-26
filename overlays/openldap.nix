8c498a501115102d901c3dda2b1357364656ce05
Butterscotch! <bscotchvanilla@gmail.com>
2026-04-26 09:06:57 -0400

    Temp fix for failing build

---
+++ overlays/openldap.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.overlays = [
+    (_: prev: {
+      openldap = prev.openldap.overrideAttrs {
+        doCheck = !prev.stdenv.hostPlatform.isi686;
+      };
+    })
+  ];
+}
