15b21860b1ab39dbb69d2664f465e8462045eba9
Butterscotch! <bscotchvanilla@gmail.com>
2026-04-28 03:47:01 -0400

    Fix trailing slash

--- hosts/lamb-desktop-2/storage.nix
+++ hosts/lamb-desktop-2/storage.nix
@@ -1,9 +1,9 @@
 {
-  fileSystems."/run/media/butterscotch/Fast Storage/" = {
+  fileSystems."/run/media/butterscotch/Fast Storage" = {
     device = "/dev/disk/by-uuid/01D92E29F0340C60";
     fsType = "ntfs3";
   };
-  fileSystems."/run/media/butterscotch/Slow Storage/" = {
+  fileSystems."/run/media/butterscotch/Slow Storage" = {
     device = "/dev/disk/by-uuid/E42CCAF82CCAC4B0";
     fsType = "ntfs3";
   };
