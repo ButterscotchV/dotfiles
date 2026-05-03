a5fb639f89a8f1ec00760c1e297a991048b9cf1e
Butterscotch! <bscotchvanilla@gmail.com>
2026-05-03 04:41:40 -0400

    Add open tablet driver

---
+++ modules/user/drawing-tablet.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+  # Enable OpenTabletDriver
+  hardware.opentabletdriver.enable = true;
+
+  # Required by OpenTabletDriver
+  hardware.uinput.enable = true;
+  boot.kernelModules = [ "uinput" ];
+}
