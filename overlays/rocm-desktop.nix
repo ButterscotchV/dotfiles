bf234ee953e45d5b3a1dfdf330517732bfabbff9
Butterscotch! <bscotchvanilla@gmail.com>
2026-05-16 23:34:12 -0400

    Update rocm-desktop.nix

--- overlays/rocm-desktop.nix
+++ overlays/rocm-desktop.nix
@@ -6,8 +6,8 @@
       rocmPackages = prev.rocmPackages.overrideScope (
         fs: ps: {
           # gfx1100 = Radeon RX 7900 XTX
-          # gfx1036 = Ryzen 7 9800X3D iGPU
-          clr = ps.clr.override { localGpuTargets = [ "gfx1100;gfx1036" ]; };
+          # gfx1036 (gfx1030) = Ryzen 7 9800X3D iGPU (may need `HSA_OVERRIDE_GFX_VERSION=10.3.0`)
+          clr = ps.clr.override { localGpuTargets = [ "gfx1100;gfx1030" ]; };
         }
       );
     })
