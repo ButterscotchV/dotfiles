0b855b50de760f8a9c65d851c117f5d2e43b682d
Butterscotch! <bscotchvanilla@gmail.com>
2026-05-18 17:41:20 -0400

    Revert kernel because of annoying Bluetooth bug

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # Configure grub
@@ -16,5 +16,16 @@
   };
 
   # Select the latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackagesFor (
+    pkgs.linuxKernel.kernels.linux_7_0.override {
+      argsOverride = rec {
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+          sha256 = "sha256-y6REQKpXr/18ISQdxbwjSw31PEmfj/w+vCkN0zkKdSM=";
+        };
+        version = "7.0.6";
+        modDirVersion = "7.0.6";
+      };
+    }
+  );
 }
