018196f83055715722730688542b557b6712bf1f
Butterscotch! <bscotchvanilla@gmail.com>
2026-05-28 02:38:37 -0400

    Add clean and repair aliases

--- home/butterscotch/shell.nix
+++ home/butterscotch/shell.nix
@@ -36,6 +36,8 @@ in
     nixos-switch = "nixos-rebuild switch --sudo";
     nixos-boot = "nixos-rebuild boot --sudo";
     nixos-size = "nix path-info --json --all --json-format 1 | jq 'map(.narSize) | add' | numfmt --to=iec-i --suffix=B";
+    nixos-clean = "nix-collect-garbage -d && sudo nix-collect-garbage -d && nix store optimise";
+    nixos-repair = "nix-store --verify --check-contents --repair";
     yt-dlp-auto = "yt-dlp --embed-subs --sub-lang 'en' -f bv*+ba/b";
     yt-dlp-auto-b = "yt-dlp-auto --cookies-from-browser firefox --user-agent '${userAgent}'";
     yt-dlp-audio = "yt-dlp -f ba -x";
