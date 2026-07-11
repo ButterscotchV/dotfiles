ac8ada66fc072abe6885a1cb00ad4e57816756a5
Butterscotch! <bscotchvanilla@gmail.com>
2026-07-11 13:45:25 -0400

    Add default applications

---
+++ home/butterscotch/default-apps.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/plain" = "code.desktop";
+      "application/json" = "code.desktop";
+      "text/markdown" = "code.desktop";
+      "text/javascript" = "code.desktop";
+      "text/css" = "code.desktop";
+      "text/typescript" = "code.desktop";
+      "text/x-kotlin" = "code.desktop";
+    };
+  };
+}
