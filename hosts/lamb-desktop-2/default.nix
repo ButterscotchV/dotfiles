af93a982492c7bd61874d577f5cff43ced7e46b7
Butterscotch! <bscotchvanilla@gmail.com>
2026-07-20 18:59:18 -0400

    Remove qdrant

--- hosts/lamb-desktop-2/default.nix
+++ hosts/lamb-desktop-2/default.nix
@@ -40,8 +40,6 @@
     pkgsLocal.insync-dolphin
   ];
 
-  services.qdrant.enable = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
