5a449835f60365af8de440175645399b0a2fb4f2
Butterscotch! <bscotchvanilla@gmail.com>
2026-07-20 22:43:57 -0400

    Update harmonia.nix

--- modules/user/harmonia.nix
+++ modules/user/harmonia.nix
@@ -1,21 +1,42 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  keyPath = "/var/lib/harmonia/lamb-desktop-2.tail11fc37.ts.net-1.secret";
+  tlsKeyPath = "/var/lib/certs/lamb-desktop-2.tail11fc37.ts.net.key";
+in
 {
+  # From https://github.com/basnijholt/dotfiles/blob/main/configs/nixos/hosts/nix-cache/harmonia.nix
+  users.groups.harmonia = { };
+  users.users.harmonia = {
+    isSystemUser = true;
+    group = "harmonia";
+    home = "/var/lib/harmonia";
+  };
+
   # Generate a public/private key pair like this:
   # $ nix-store --generate-binary-cache-key cache.yourdomain.tld-1 /var/lib/secrets/harmonia.secret /var/lib/secrets/harmonia.pub
   services.harmonia = {
     cache = {
       enable = true;
-      signKeyPaths = [ "/var/lib/secrets/harmonia.secret" ];
+      signKeyPaths = [ keyPath ];
       settings = {
         bind = "[::]:49023";
-        tls_cert_path = "/home/butterscotch/Certs/lamb-desktop-2.tail11fc37.ts.net.crt";
-        tls_key_path = "/home/butterscotch/Certs/lamb-desktop-2.tail11fc37.ts.net.key";
+        tls_cert_path = "/var/lib/certs/lamb-desktop-2.tail11fc37.ts.net.crt";
+        tls_key_path = tlsKeyPath;
       };
     };
 
     daemon.enable = true;
   };
+
+  systemd.services.harmonia.serviceConfig.DynamicUser = lib.mkForce false;
+
+  # Ensure Harmonia can read the persistent signing key across service restarts
+  systemd.tmpfiles.rules = [
+    "d /var/lib/harmonia 0750 harmonia harmonia -"
+    "z ${keyPath} 0600 harmonia harmonia -"
+    "z ${tlsKeyPath} 0600 harmonia harmonia -"
+  ];
 
   networking.firewall.allowedTCPPorts = [ 49023 ];
 }
