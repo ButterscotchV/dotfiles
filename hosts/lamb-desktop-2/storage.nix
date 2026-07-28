86b00ee5fc346d0f9c7329a30e278db97e60b051
Butterscotch! <bscotchvanilla@gmail.com>
2026-07-28 00:18:35 -0400

    Change Slow Storage to ext4 & use nofail

--- hosts/lamb-desktop-2/storage.nix
+++ hosts/lamb-desktop-2/storage.nix
@@ -2,9 +2,11 @@
   fileSystems."/run/media/butterscotch/Fast Storage" = {
     device = "/dev/disk/by-uuid/01D92E29F0340C60";
     fsType = "ntfs3";
+    options = [ "nofail" ];
   };
   fileSystems."/run/media/butterscotch/Slow Storage" = {
-    device = "/dev/disk/by-uuid/E42CCAF82CCAC4B0";
-    fsType = "ntfs3";
+    device = "/dev/disk/by-uuid/12664d5d-d2bd-4b91-955b-64a0b131fe3f";
+    fsType = "ext4";
+    options = [ "nofail" ];
   };
 }
